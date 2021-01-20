1639df27664f2aa2c5c1a1412e28d51d23111bfa
gabriele <gabrielescodinu96@gmail.com>
2021-01-20 10:39:31 +0100

    Introducing Vue Js For Links and Brands

---
+++ main.js
@@ -0,0 +1,14 @@
+let app = new Vue ({
+  el: "#app",
+  data: {
+    navbarLinks: ["Home", "About Me", "Testimonials", "My Blog", "Meetups", "Shop", "Contact Me"],
+    creditLinks: ["Latest Book", "Upcoming Events", "Recent Articles", "Buisness Enquiries", "Visit My Fundation"],
+    brands: [
+      "ebay-logotan-2x-200x66.png",
+      "audible-logotan-2x-200x66.png",
+      "bb-logotan-2x-200x66.png",
+      "kindlefire-logotan-2x-200x66.png",
+    ],
+    
+  }
+})
